60d30238554954336dd3884bf255e5686655f715
K3ndev <racelisjk@gmail.com>
2023-07-22 16:56:56 +0800

    feat: added useOs hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,7 @@
-export { useDebounce } from "./useDebouncedState/index";
 export { useAos } from "./useAos";
+export { useContainerPosition } from "./useContainerPosition";
+export { useDebounce } from "./useDebouncedState/index";
+export { useIsDivVisible } from "./useIsDivVisible";
+export { useOs } from "./useOs";
+export { useResponsiveSize } from "./useResponsiveSize";
 export { useWScrollY } from "./useWScrollY";
---
+++ src/hooks/useOs/index.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Custom hook that returns the operating system of the user.
+ *
+ * @returns {string} - A string value indicating the operating system of the user, such as "Linux", "Windows", "MacIntel", "Android", or "undetermined" if the information is not available.
+ * @example
+ * // Usage example:
+ * const os = useOs();
+ */
+export function useOs() {
+  const [os, setOs] = useState("");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Check if the property is available before accessing it
+      if (window.navigator && window.navigator.platform) {
+        const firstWord = window.navigator.platform.split(" ")[0];
+        setOs(firstWord);
+      } else {
+        console.log("Platform information not available.");
+        setOs("undetermined");
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return os;
+}
